45ede565027870ebeae40b3c28e38ba087f1fcf8
Pierre Thelusma <pierrethelusma@Pierres-MacBook-Pro.local>
2013-10-11 15:29:32 -0400

    Initial Commit

---
+++ SPoT/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SPoT
+//
+//  Created by Pierre Thelusma on 10/11/13.
+//  Copyright (c) 2013 Pierre Thelusma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ SPoT/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SPoT
+//
+//  Created by Pierre Thelusma on 10/11/13.
+//  Copyright (c) 2013 Pierre Thelusma. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
